faedace29711a4a9f7efafacba190f335fdf6751
Nathan W <nwinslow@gmail.com>
2026-03-09 12:41:44 -0500

    created sandbox-tags.js and updated main.js

--- sandbox-main.js
+++ sandbox-main.js
@@ -1,5 +1,6 @@
 import { initModal } from './sandbox-modal.js';
 import {initComments} from './sandbox-comments.js';
+import {tags} from './sandbox-tags.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     console.log("DOM fully loaded and parsed");
