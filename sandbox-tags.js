e16d952f27446d91d5cb876b51161e22c4fb21ca
Nathan W <nwinslow@gmail.com>
2026-03-09 20:15:41 -0500

    working on extractTags func

---
+++ sandbox-tags.js
@@ -0,0 +1,38 @@
+ const tags = { // create tag object
+    extractTags(text) {
+        //grab the comment
+        const text = document.querySelector('.comment-input');
+            // verify the input is not empty
+        if(text.value === true && text.length > 2) {
+            // put comment into an array
+            const commentArray = [];
+            commentArray.push(text);
+            // scan each index (word) for a '#'
+            commentArray.forEach()
+
+        } else {
+            return;
+        }
+    },
+
+    normalizeTag(tag){
+
+    },
+
+    searchCommentsByTag(comments,search) {
+
+    },
+
+    hasTag(comment,tag) {
+
+    },
+
+    getAllTags(comments) {
+
+    },
+
+    renderTags(tags) {
+
+    }
+}
+
